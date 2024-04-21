aa26f4b8f4c63a4dfdbefc45e318ef20d391447e
Camilo Montoya :) <camilomontoya2@macmillan.com>
2024-04-21 15:31:33 -0500

    chore: Add server setup and Pact verification in provider test

--- pact-testing/src/api/provider/provider.test.ts
+++ pact-testing/src/api/provider/provider.test.ts
@@ -2,12 +2,37 @@ import { Verifier } from '@pact-foundation/pact';
 
 import { PROVIDER_NAME, pactFile } from '../pact';
 
+import { app } from '../../server';
+
+
 
 
 const HOST_NAME = '127.0.0.1';
 const PORT = 3000;
 
 describe('Pact verfication', ()=>{
-  
+  before(()=> {
+    app.listen(PORT, HOST_NAME, ()=>{
+      console.log(`Server is running on http://${HOST_NAME}:${PORT}`);
+    })
+  })  
+  after(()=> {
+    app.close();
+  })
 
+  it('should validate the pact', async () => {
+    return await new Verifier({
+      provider: PROVIDER_NAME,
+      providerBaseUrl: `http://${HOST_NAME}:${PORT}`,
+      pactUrls: [pactFile],
+    }).verifyProvider()
+    .then(output => {
+      console.log('Pact Verification Complete!')
+      console.log(output)
+    })
+    .catch(e => {
+      console.log('Pact Verification Failed!')
+      console.log(e)
+    })
+  });
 });
