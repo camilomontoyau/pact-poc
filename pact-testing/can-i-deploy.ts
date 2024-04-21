4cbda8eae12acb11d57e79e7b557e991a6f87705
Camilo Montoya :) <camilomontoya2@macmillan.com>
2024-04-21 14:09:29 -0500

    checkpoint

--- pact-testing/can-i-deploy.ts
+++ pact-testing/can-i-deploy.ts
@@ -5,14 +5,14 @@ const PACT_BROKER_URL = process.env.PACT_BROKER_URL ?? 'http://localhost:9292';
 const PACT_BROKER_PASSWORD = process.env.PACT_BROKER_PASSWORD ?? 'password';
 const PACT_BROKER_USERNAME = process.env.PACT_BROKER_USERNAME ?? 'user';
 const PACTICIPANT = process.env.PACTICIPANT ?? 'myConsumer' ;
-const PACTICIPANT_VERSION = process.env.PACTICIPANT_VERSION ;
+const PACTICIPANT_VERSION = process.env.PACTICIPANT_VERSION ?? '123456';
 
 console.log('here');
 pact.canDeploy({
   pacticipants: [
     {
       name: PACTICIPANT,
-      version: PACTICIPANT_VERSION
+      version: PACTICIPANT_VERSION,
     }
   ],
   pactBroker: PACT_BROKER_URL,
@@ -21,6 +21,7 @@ pact.canDeploy({
   output: 'json',
   retryWhileUnknown: 5,
   retryInterval: 10,
+  verbose: true
 }).then((result) => {
   console.log(JSON.stringify({ result }, null, 2));
   console.log('it succeeded');
